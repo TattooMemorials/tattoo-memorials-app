48e925808b636ddbfc7b20f1783007c237f235b2
Dan Tinsman <tinsman@Dans-MacBook-Pro.local>
2024-09-20 22:04:22 -0400

    update email

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -43,7 +43,7 @@ export async function POST(request: Request) {
 
         // Send email
         await postmarkClient.sendEmail({
-            From: "dan@tinner.tech",
+            From: "communications@tattoomemorials.com",
             To: email,
             Subject: subject,
             HtmlBody: message,
